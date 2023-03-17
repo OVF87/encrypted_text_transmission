6ff124cc82b2840cc620649b84ab4063d9b31043
OVF <okarskiy@gmail.com>
2023-03-17 11:58:50 +0400

    add resize vidgets frames

--- gui.py
+++ gui.py
@@ -6,37 +6,35 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 class Gui:
     '''Класс создает графический интерфейс'''
-    def __init__(self, title='Передача зашифрованных файлов', my_ip=None):
-        self.root = tk.Tk()
-        self.root.title(title)
-        self.root.main_menu = tk.Menu()
-        self.root.file_menu = tk.Menu(tearoff=0)
-        self.root.file_menu.add_command(label="New", command=self.new_text)
-        self.root.file_menu.add_command(label="Save", command=self.save_file)
-        self.root.file_menu.add_command(label="Open", command=self.open_file)
-        self.root.file_menu.add_separator()
-        self.root.file_menu.add_command(label="Exit", command=self.root.destroy)
-
-        self.root.main_menu.add_cascade(label="File", menu=self.root.file_menu)
-        self.root.main_menu.add_cascade(label="Help", command=self.get_help)
-
-
-
-
+    def __init__(self, my_ip=None):
+        self.title='Передача зашифрованных файлов'
         self.my_ip = my_ip
         self.out_ip = 0
 
-        self.root.rowconfigure(0, minsize=100, weight=10)
-        self.root.columnconfigure([0, 1], minsize=100, weight=10)
+        self.root = tk.Tk()
+        self.root.title(self.title)
+
+        self.root.main_menu = tk.Menu()
+        self.root.file_menu = tk.Menu(tearoff=0)
+        self.root.file_menu.add_command(label='New', command=self.new_text)
+        self.root.file_menu.add_command(label='Save', command=self.save_file)
+        self.root.file_menu.add_command(label='Open', command=self.open_file)
+        self.root.file_menu.add_separator()
+        self.root.file_menu.add_command(label='Exit', command=self.root.destroy)
+        self.root.main_menu.add_cascade(label='File', menu=self.root.file_menu)
+        self.root.main_menu.add_cascade(label='Help', command=self.get_help)
+
+        self.root.rowconfigure(0, minsize=100, weight=1)
+        self.root.columnconfigure(1, minsize=100, weight=1)
 
         self.root.btn_frame = tk.Frame(self.root, relief=tk.RAISED, bd=2)
-        self.root.btn_frame.rowconfigure([i for i in range(14)], minsize=5, weight=10)
-        self.root.btn_frame.columnconfigure([i for i in range(3)], minsize=50, weight=10)
+        self.root.btn_frame.rowconfigure([i for i in range(14)], minsize=5)
+        self.root.btn_frame.columnconfigure([i for i in range(3)], minsize=30)
 
         self.lb_top = tk.Label(self.root.btn_frame, text='Для поиска в локальной сети нажми search')
         self.lb_my_ip = tk.Label(self.root.btn_frame, text=f'Мой IP:\n{self.my_ip}')
-        self.lb_out_ip = tk.Label(self.root.btn_frame, text=f'out IP:\n{self.out_ip}')
-        self.lb_info = tk.Label(self.root.btn_frame, text='')
+        self.lb_out_ip = tk.Label(self.root.btn_frame, text=f'Чужой IP:\n{self.out_ip}')
+        self.lb_info = tk.Label(self.root.btn_frame, text='...')
 
         self.btn_search = tk.Button(self.root.btn_frame, text='Поиск в локальной сети', command=self.search)
         self.btn_plug = tk.Button(self.root.btn_frame, text='Принять данные',)
@@ -44,8 +42,11 @@ class Gui:
         self.btn_encypt = tk.Button(self.root.btn_frame, text='Зашифровать',)
         self.btn_decrypt = tk.Button(self.root.btn_frame, text='Расшифровать',)
 
-        self.txt_edit = tk.Text(self.root)
-        self.lb_out_text = tk.Label(self.root, text=None, bg='yellow')
+        self.root.text_frame = tk.Frame(self.root, relief=tk.RAISED, bd=2)
+        self.root.text_frame.rowconfigure([0, 1], minsize=50)
+        self.root.text_frame.columnconfigure(0, minsize=50, weight=1)
+        self.txt_edit = tk.Text(self.root.text_frame)
+        self.lb_out_text = tk.Label(self.root.text_frame, text='...Здесь будет принятый текст...', bg='yellow')
 
     def call_gui(self):
         '''Вызов окна программы'''
@@ -54,6 +55,7 @@ class Gui:
         self.root.mainloop()
 
     def draw_widgets(self):
+        '''Прорисовка виджетов'''
         self.root.btn_frame.grid(row=0, column=0, sticky='nsew')
 
         self.lb_top.grid(row=0, column=1,  sticky='ew', padx=5)
@@ -67,11 +69,12 @@ class Gui:
         self.btn_encypt.grid(row=6, column=1, sticky='ew', padx=5)
         self.btn_decrypt.grid(row=7, column=1, sticky='ew', padx=5)
 
-        self.txt_edit.grid(row=0, column=1, sticky='ew')
-        self.lb_out_text.grid(row=1, column=1,sticky='n')
+        self.root.text_frame.grid(row=0, column=1, sticky='nsew')
+        self.txt_edit.grid(row=0, column=0, sticky='nsew')
+        self.lb_out_text.grid(row=1, column=0, sticky='nsew')
 
     def open_file(self):
-        '''Открываем файл для редактирования'''
+        '''Открываем текстовый файл'''
         filepath = askopenfilename(
             filetypes = [('Текстовые файлы', '*.txt'), ('Все файлы', '*.*')])
         if not filepath:
@@ -80,10 +83,10 @@ class Gui:
         with open(filepath, 'r') as input_file:
             text = input_file.read()
             self.txt_edit.insert(tk.END, text)
-        self.root.title(f'file - {filepath}')
+        self.root.title(self.title + f'\t - {filepath}')
 
     def save_file(self):
-        '''Сохраняем текущий файл как новый файл'''
+        '''Сохраняем текущий текстовый файл'''
         filepath = asksaveasfilename(
             defaultextension = 'txt',
             filetypes = [('Текстовые файлы', '*.txt'), ('Все файлы', '*.*')],)
@@ -92,9 +95,10 @@ class Gui:
         with open(filepath, 'w') as output_file:
             text = self.txt_edit.get('1.0', tk.END)
             output_file.write(text)
-        self.root.title(f'текстовый редактор - {filepath}')
+        self.root.title(self.title + f'\t - {filepath}')
 
     def get_help(self):
+        '''Вызов окна справки'''
         help_window = tk.Tk()
         help_window.title('Help')
         help_window.rowconfigure(0, minsize=100, weight=10)
@@ -103,21 +107,27 @@ class Gui:
         lb_help.grid()
 
     def new_text(self):
+        '''Очистка поля ввода текста'''
         self.txt_edit.delete('1.0', tk.END)
 
     def search(self):
+        '''Поиск другого экземпляра программы'''
         pass
 
-    def plug(self):
+    def get_receiving(self):
+        '''Прием данных'''
         pass
 
-    def send(self, ):
+    def get_send(self, ):
+        '''Отправка данных'''
         pass
 
-    def encrypt(self):
+    def get_encrypt(self):
+        '''Шифрование данных'''
         pass
 
-    def decrypt(self):
+    def get_decrypt(self):
+        '''Расшифровка данных'''
         pass
 
 
