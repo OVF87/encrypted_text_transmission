0b3fbeee17a5aa289ec5ae6ff97b2981f94711f1
OVF <okarskiy@gmail.com>
2023-03-17 10:08:10 +0400

    create class gui

---
+++ gui.py
@@ -0,0 +1,79 @@
+# Инициализация класса графического интерфейса
+
+import tkinter as tk
+
+
+class Gui:
+    '''Класс создает графический интерфейс'''
+    def __init__(self, title='Передача файлов', my_ip=None):
+        self.root = tk.Tk()
+        self.root.title(title)
+        self.my_ip = my_ip
+        self.out_ip = 0
+
+        self.root.rowconfigure(0, minsize=100, weight=10)
+        self.root.columnconfigure([0, 1], minsize=100, weight=10)
+
+        self.root.btn_frame = tk.Frame(self.root, relief=tk.RAISED, bd=2)
+        self.root.btn_frame.rowconfigure([i for i in range(14)], minsize=5, weight=10)
+        self.root.btn_frame.columnconfigure([i for i in range(3)], minsize=50, weight=10)
+
+        self.lb_top = tk.Label(self.root.btn_frame, text='Для поиска в локальной сети нажми search')
+        self.lb_my_ip = tk.Label(self.root.btn_frame, text=f'Мой IP:\n{self.my_ip}')
+        self.lb_out_ip = tk.Label(self.root.btn_frame, text=f'out IP:\n{self.out_ip}')
+        self.lb_info = tk.Label(self.root.btn_frame, text='')
+
+        self.btn_search = tk.Button(self.root.btn_frame, text='Поиск в локальной сети', command=self.search)
+        self.btn_plug = tk.Button(self.root.btn_frame, text='Принять данные',)
+        self.btn_send = tk.Button(self.root.btn_frame, text='Отправить данные',)
+        self.btn_encypt = tk.Button(self.root.btn_frame, text='Зашифровать',)
+        self.btn_decrypt = tk.Button(self.root.btn_frame, text='Расшифровать',)
+
+        self.txt_edit = tk.Text(self.root)
+        self.lb_out_text = tk.Label(self.root, text=None, bg='yellow')
+
+    def call_gui(self):
+        '''Вызов окна программы'''
+        self.draw_widgets()
+        self.root.mainloop()
+
+    def draw_widgets(self):
+        self.root.btn_frame.grid(row=0, column=0, sticky='nsew')
+
+        self.lb_top.grid(row=0, column=1,  sticky='ew', padx=5)
+        self.btn_search.grid(row=1, column=1, sticky='ew', padx=5)
+        self.lb_my_ip.grid(row=2, column=0, sticky='ew', padx=5)
+        self.lb_out_ip.grid(row=2, column=2, sticky='ew', padx=5)
+        self.lb_info.grid(row=2, column=1, sticky='ew', padx=5)
+
+        self.btn_plug.grid(row=2, column=1, sticky='ew', padx=5)
+        self.btn_send.grid(row=3, column=1, sticky='ew', padx=5)
+        self.btn_encypt.grid(row=6, column=1, sticky='ew', padx=5)
+        self.btn_decrypt.grid(row=7, column=1, sticky='ew', padx=5)
+
+        self.txt_edit.grid(row=0, column=1, sticky='ew')
+        self.lb_out_text.grid(row=1, column=1,sticky='n')
+
+    def search(self):
+        pass
+
+    def plug(self):
+        pass
+
+    def send(self, ):
+        pass
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    window = Gui()
+    window.call_gui()# Напишите здесь свой код :-)
