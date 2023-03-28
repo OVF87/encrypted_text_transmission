01e28678ac7bae5ebd43dca0b2ec80e7d4660c3e
OVF <okarskiy@gmail.com>
2023-03-28 08:36:38 +0400

    refactoriing

--- gui.py
+++ gui.py
@@ -1,12 +1,9 @@
 # Инициализация класса графического интерфейса
-
+import random  # !!!!!!!!!!!!!!
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from search import Search
-from network import Networking
-from cripto import Cripto
 import variables as var
-import random  # !!!!!!!!!!!!!!
+
 
 
 class Gui:
@@ -15,15 +12,22 @@ class Gui:
     '''
     def __init__(self, my_ip=None):
         self.title='Передача зашифрованных файлов'
-        self.my_ip = random.getrandbits(64) # !!!!!!!!!! my_ip
-        self.out_ip = 0
-        self.crp = Cripto()
-        self.my_n, self.my_public_key, self.my_secret_key = self.crp.get_keys()
+        self.my_id = None
+        self.my_ip = var.MY_ADDRESS
+        self.out_id = None
+        self.out_ip = None
+        self.my_n = None
+        self.my_public_key = None
+        self.my_secret_key = None
         self.out_n = None
         self.out_public_key = None
         self.root = tk.Tk()
         self.root.title(self.title)
 
+    def create_menu(self):
+        '''
+        Создание меню
+        '''
         self.root.main_menu = tk.Menu()
         self.root.file_menu = tk.Menu(tearoff=0)
         self.root.file_menu.add_command(label='New', command=self.new_text)
@@ -33,38 +37,42 @@ class Gui:
         self.root.file_menu.add_command(label='Exit', command=self.root.destroy)
         self.root.main_menu.add_cascade(label='File', menu=self.root.file_menu)
         self.root.main_menu.add_cascade(label='Help', command=self.get_help)
+        self.root.config(menu=self.root.main_menu)
 
-        self.root.rowconfigure(0, minsize=100, weight=1)
-        self.root.columnconfigure(1, minsize=100, weight=1)
-
+    def create_widget(self):
+        '''
+        Создает виджеты кнопок, информации, текстовые поля
+        '''
+        #  Создание и конфигурация рамки для виджетов кнопок и текстовых меток
         self.root.btn_frame = tk.Frame(self.root, relief=tk.RAISED, bd=2)
         self.root.btn_frame.rowconfigure([i for i in range(14)], minsize=5)
         self.root.btn_frame.columnconfigure([i for i in range(3)], minsize=30)
 
-        self.lb_top = tk.Label(self.root.btn_frame, text='Для поиска в локальной сети нажми search')
+        #  Создание текстовых меток о своей информации
         self.lb_my_ip = tk.Label(self.root.btn_frame, text=f'Мой IP:\n{self.my_ip}')
-        self.lb_out_ip = tk.Label(self.root.btn_frame, text=f'Чужой IP:\n{self.out_ip}')
-        self.lb_info = tk.Label(self.root.btn_frame, text='...')
-
+        self.lb_my_id = tk.Label(self.root.btn_frame, text=f'Мой ID:\n{self.my_id}')
         self.lb_my_n = tk.Label(self.root.btn_frame, text=f'Мой n:\n{self.my_n}')
         self.lb_my_public_key = tk.Label(self.root.btn_frame, text=f'Мой public_key:\n{self.my_public_key}')
         self.lb_my_secret_key = tk.Label(self.root.btn_frame, text=f'Мой secret_key:\n{self.my_secret_key}')
+
+        #  Создание текстовых меток о чужой информации
+        self.lb_out_ip = tk.Label(self.root.btn_frame, text=f'Чужой IP:\n{self.out_ip}')
+        self.lb_out_id = tk.Label(self.root.btn_frame, text=f'Чужой ID:\n{self.out_id}')
         self.lb_out_n = tk.Label(self.root.btn_frame, text=f'Чужой n:\n{self.out_n}')
         self.lb_out_public_key = tk.Label(self.root.btn_frame, text=f'Чужой public_key:\n{self.out_public_key}')
 
-        self.btn_search = tk.Button(self.root.btn_frame, text='Поиск в локальной сети', command=self.search)
-        self.btn_plug = tk.Button(self.root.btn_frame, text='Принять данные', command=self.get_receiving)
-        self.btn_send = tk.Button(self.root.btn_frame, text='Отправить данные', command=self.get_send)
-        self.btn_encypt = tk.Button(self.root.btn_frame, text='Зашифровать', command=self.get_encrypt)
-        self.btn_decrypt = tk.Button(self.root.btn_frame, text='Расшифровать', command=self.get_decrypt)
+        #  Создание кнопок
+        self.btn_send = tk.Button(self.root.btn_frame, text='Отправить данные')#, command=self.get_send)
+        self.btn_encrypt = tk.Button(self.root.btn_frame, text='Зашифровать')
+        self.btn_decrypt = tk.Button(self.root.btn_frame, text='Расшифровать')
 
+        #  Создание текстовых полей
         self.root.text_frame = tk.Frame(self.root, relief=tk.RAISED, bd=2)
         self.root.text_frame.rowconfigure([0, 1, 2], minsize=20)
         self.root.text_frame.columnconfigure(0, minsize=20, weight=1)
         self.msg_list = tk.Listbox(self.root.text_frame, height=20, width=30)
         self.y_scrollbar_msg_list = tk.Scrollbar(self.root.text_frame, command=self.msg_list.yview)
         self.x_scrollbar_msg_list = tk.Scrollbar(self.root.text_frame, orient="horizontal", command=self.msg_list.xview)
-
         self.msg_list.configure(xscrollcommand=self.x_scrollbar_msg_list.set,  yscrollcommand=self.y_scrollbar_msg_list.set)
         self.scrollbar_text_edit = tk.Scrollbar(self.root.text_frame)
         self.txt_edit = tk.Text(self.root.text_frame, yscrollcommand=self.scrollbar_text_edit.set)
@@ -74,33 +82,38 @@ class Gui:
         '''
         Вызов окна программы
         '''
-        self.root.config(menu=self.root.main_menu)
+
+        self.create_menu()
+        #self.create_widget()
         self.draw_widgets()
         self.root.mainloop()
 
     def draw_widgets(self):
         '''
-        Прорисовка виджетов
+        Размещает виджеты
         '''
+        #  Рамка кнопок
         self.root.btn_frame.grid(row=0, column=0, sticky='nsew')
 
-        self.lb_top.grid(row=0, column=1,  sticky='ew', padx=5)
-        self.btn_search.grid(row=1, column=1, sticky='ew', padx=5)
+        #  Текстовые метки своей информации
+        self.lb_my_id.grid(row=1, column=0, sticky='ew', padx=5)
         self.lb_my_ip.grid(row=2, column=0, sticky='ew', padx=5)
-        self.lb_out_ip.grid(row=2, column=2, sticky='ew', padx=5)
-        self.lb_info.grid(row=2, column=1, sticky='ew', padx=5)
-
-        self.btn_plug.grid(row=2, column=1, sticky='ew', padx=5)
-        self.btn_send.grid(row=3, column=1, sticky='ew', padx=5)
-        self.btn_encypt.grid(row=6, column=1, sticky='ew', padx=5)
-        self.btn_decrypt.grid(row=7, column=1, sticky='ew', padx=5)
-
         self.lb_my_n.grid(row=3, column=0, sticky='ew', padx=5)
         self.lb_my_public_key.grid(row=4, column=0, sticky='ew', padx=5)
         self.lb_my_secret_key.grid(row=5, column=0, sticky='ew', padx=5)
+
+        #  Текстовые метки чужой информации
+        self.lb_out_id.grid(row=1, column=2, sticky='ew', padx=5)
+        self.lb_out_ip.grid(row=2, column=2, sticky='ew', padx=5)
         self.lb_out_n.grid(row=3, column=2, sticky='ew', padx=5)
         self.lb_out_public_key.grid(row=4, column=2, sticky='ew', padx=5)
 
+        #  Виджеты кнопок
+        self.btn_send.grid(row=3, column=1, sticky='ew', padx=5)
+        self.btn_encrypt.grid(row=6, column=1, sticky='ew', padx=5)
+        self.btn_decrypt.grid(row=7, column=1, sticky='ew', padx=5)
+
+        #  Виджеты тектовых полей
         self.root.text_frame.grid(row=0, column=1, sticky='nsew')
         self.y_scrollbar_msg_list.grid(row=0, column=2, sticky='ns')
         self.x_scrollbar_msg_list.grid(row=1, column=0, sticky='sew')
@@ -154,19 +167,8 @@ class Gui:
         '''
         self.txt_edit.delete('1.0', tk.END)
 
-    def search(self):
-        '''
-        Поиск другого экземпляра программы
-        '''
-        print('search is running')
-        self.lb_info.configure(text='Поиск начат...')
-        sr = Search(self.my_ip, self.my_n, self.my_public_key).run()
-        self.lb_out_ip.configure(text=f'Чужой IP:\n{sr[1]}')
-        self.out_ip, self.out_n, self.out_public_key = sr[1:]
-        self.lb_out_n.configure(text=f'Чужой n:\n{self.out_n}')
-        self.lb_out_public_key.configure(text=f'Чужой public_key:\n{self.out_public_key}')
-        print('out ip: ', self.out_ip)
-        self.lb_info.configure(text='...')
+    def get_out_data(self):
+        pass
 
     def get_receiving(self):
         '''
@@ -191,7 +193,7 @@ class Gui:
         connect = Networking('TCP')
         connect.send_data(temp.encode())
         self.lb_info.configure(text='...')
-
+    """
     def get_encrypt(self):
         '''
         Шифрование данных
@@ -216,13 +218,20 @@ class Gui:
         self.lb_out_text.configure(text=text)
         print(text)
         self.lb_info.configure(text='...')
+    """
+    def get_text(self) -> str:
+        text = self.txt_edit.get('1.0', tk.END)
+        return text
 
+    def pull_text(self, text: str):
+        self.txt_edit.delete('1.0', tk.END)
+        self.txt_edit.insert(tk.END, text)
 
-
-
-
+    def add_msg_in_list(self, text: str):
+        self.msg_list.insert(tk.END, text)
 
 
 if __name__ == '__main__':
     window = Gui()
-    window.call_gui()# Напишите здесь свой код :-)
+    window.create_widget()
+    window.call_gui()
