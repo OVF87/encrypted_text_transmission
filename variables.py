213dc9a8377145c8aa673530881d37c2a2a7d3b5
OVF <okarskiy@gmail.com>
2023-03-28 16:00:32 +0400

    refactoring

--- variables.py
+++ variables.py
@@ -4,6 +4,6 @@ import socket
 host= socket.getaddrinfo(socket.gethostname(), None)
 
 PORT_NO = [37020, 37021]
-TIME_OUT = 20.0  # время таймаута при ожидании данных в сокет
+TIME_OUT = 3.0  # время таймаута при ожидании данных в сокет
 BUFFER_SIZE = 1024  # размер буфера для примем сообщений
 MY_ADDRESS = host[1][4][0]
