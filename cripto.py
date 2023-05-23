ee04f26710d225ff2bf3afa700d4d6de91b45199
OVF <okarskiy@gmail.com>
2023-05-23 11:00:10 +0400

    refactoring

--- cripto.py
+++ cripto.py
@@ -5,45 +5,56 @@ import time
 
 
 class Cripto():
-    '''Генерирует приватный и публичный ключи, шифрует и дешифрует текст'''
+    '''
+    Генерирует приватный и публичный ключи, шифрует и дешифрует текст
+    '''
     def __init__(self):
         self.START_GENERATOR = 65 * pow(10, 7)
         self.END_GENERATOR = 75 * pow(10, 7)
 
     def get_decrypt(self, data: str, d: int, n: int) -> str:
-        '''Дешифрует текст'''
+        '''
+        Дешифрует текст
+        :param data: текст сообщения
+        :param d: секретный ключ
+        :param n: модуль 
+        '''
         result = ''
         for j in range(3, len(data), 22):
             temp = int(data[j:j+19])
-            #print('dec do', temp, end='    ')
             temp = pow(temp, d, n)
-            #print('dec posle', temp, chr(temp))
             result += chr(temp)
-        #print ('Plain Text is: ', result)
         return result
 
     def get_encrypt(self, text: str, e: int, n: int) -> str:
-        '''Шифрует текст'''
+        '''
+        Шифрует текст
+        :param data: текст сообщения
+        :param e: публичный ключ
+        :param n: модуль - произведение двух простых, не равных чисел
+        '''
         result = ''
         for i in range(0,len(text)):
             temp = str(pow(ord(text[i]), e, n))
-            #print('len enc do', len(temp), temp, end='    ')
             temp = temp.zfill(19)
             while len(temp) < 22:
                 temp =  str(random.randint(1, 9)) + temp
-            #print('len enc posle', len(temp), temp)
             result += temp
-        #print ('Cipher Text is: ', result)
         return result
 
     def get_gcd(self, a: int, b: int) -> int:
-        '''Находит наибольший общий делитель для чисел a и b'''
+        '''
+        Находит наибольший общий делитель для чисел a и b
+        :param a, b: простые числа a != b
+        '''
         if(b == 0):
             return a
         return self.get_gcd(b, a % b)
 
     def get_keys(self) -> tuple:
-        '''Генерирует кортедж чисел для шифрования/дешифрования'''
+        '''
+        Генерирует кортедж чисел для шифрования/дешифрования
+        '''
         p, q = self.get_prime_pair()
         n = p * q
         phi = (p - 1) * (q - 1)
@@ -52,7 +63,9 @@ class Cripto():
         return n, public_key, secret_key
 
     def get_prime_pair(self) -> tuple:
-        '''Генерирует пару простых чисел'''
+        '''
+        Генерирует пару простых чисел
+        '''
         while 1:
             while 1:
                 p = random.randint(self.START_GENERATOR, self.END_GENERATOR)
@@ -67,7 +80,11 @@ class Cripto():
         return p, q
 
     def get_public_key(self, phi: int) -> int:
-        '''Генерирует публичный ключ'''
+        '''
+        Генерирует публичный ключ
+        :param phi: Функция Эйлера – функция, равная количеству натуральных
+        чисел, меньших n и взаимно простых с ним
+        '''
         while 1:
             public_key = random.randrange(1, phi)
             if self.get_gcd(public_key, phi) == 1:
@@ -75,12 +92,18 @@ class Cripto():
         return public_key
 
     def get_secret_key(self, public_key: int, phi: int) -> int:
-        '''Генерирует приватный ключ'''
+        '''
+        Генерирует приватный ключ
+        :param phi: Функция Эйлера – функция, равная количеству натуральных
+        чисел, меньших n и взаимно простых с ним
+        '''
         secret_key = self.multiplicative_inverse(public_key, phi)
         return secret_key
 
     def is_prime(self, n: int) -> bool:
-        '''Проверка на простое число'''
+        '''
+        Проверка на простое число
+        '''
         if n == 1:
             return False
         for i in range(2, int(math.sqrt(n))+1):
@@ -89,7 +112,12 @@ class Cripto():
         return True
 
     def multiplicative_inverse(self, e: int, phi: int) -> int:
-        '''Генерация приватного ключа'''
+        '''
+        Генерация приватного ключа
+        :param e: публичный ключ
+        :param phi: Функция Эйлера – функция, равная количеству натуральных
+        чисел, меньших n и взаимно простых с ним
+        '''
         u1, u2, u3 = 1, 0, e
         v1, v2, v3 = 0, 1, phi
         while v3 != 0:
